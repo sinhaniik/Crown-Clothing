eca26d3792f42f1fb90e91788100e5448305f92c
Nikhil Sinha <nikhilsinha198@gmail.com>
2022-07-04 11:31:39 +0530

    introduction to reselect make our createSelector work more efficient

--- src/component/store/categories/categories.selector.js
+++ src/component/store/categories/categories.selector.js
@@ -1,7 +1,21 @@
-export const selectCategoriesMap = (state) =>
-	state.categories.categories.reduce((acc, category) => {
-		const { title, items } = category;
-		// console.log(docSnapshot.data());
-		acc[title.toLowerCase()] = items;
-		return acc;
-	}, {});
+import { createSelector } from 'reselect';
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+	//initial state
+	[selectCategoryReducer],
+	//changed state
+	(categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+	[selectCategories],
+	(categories) =>
+		categories.reduce((acc, category) => {
+			const { title, items } = category;
+			// console.log(docSnapshot.data());
+			acc[title.toLowerCase()] = items;
+			return acc;
+		}, {})
+);
